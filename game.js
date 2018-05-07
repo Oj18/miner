c63075e078a93916bc2e8088cc6db7e1e65b52b8
Oj18 <ojmed1803@gmail.com>
2018-05-07 19:54:48 +0100

    v0.4.0 - See changelog

--- game.js
+++ game.js
@@ -10,6 +10,10 @@ var shopallowed = false;
 var shopcount = 0;
 var shopcost = 200;
 var exchange = 1;
+var canrefine = false;
+var refined = 0;
+var refinecost = 10;
+var objective = 0;
 
 function update() {
 	if (hasautosell) {
@@ -34,30 +38,67 @@ function update() {
 	
 	document.getElementById("exchange").innerHTML = "Exchange rate: 1 material = $" + exchange.toFixed(10);
 	
-	document.getElementById("material-count").innerHTML = materials + " materials";
+	if (materials == 1) {
+		document.getElementById("material-count").innerHTML = "0 material";
+	} else {
+		document.getElementById("material-count").innerHTML = materials + " materials";
+	}
+	
 	document.getElementById("money-count").innerHTML = "$" + money.toFixed(2);
 	document.getElementById("worker-cost").innerHTML = "Costs: $" + workercost;
 	
+	document.getElementById("refine-cost").innerHTML = "Refinement: Costs " + refinecost + " materials";
+	
 	if (workercount == 1) {
+		$("#worker-count").fadeIn("slow");
+		
 		document.getElementById("worker-count").innerHTML = "1 worker";
 	} else {
-		document.getElementById("worker-count").innerHTML = workercount + " workers";
+		if (workercount != 0) {
+			$("#worker-count").fadeIn("slow");
+			
+			document.getElementById("worker-count").innerHTML = workercount + " workers";
+		} else {
+			document.getElementById("worker-count").hidden = true;
+		}
 	}
 	
 	if (shopallowed) {
 		$("#shop-container").fadeIn("slow");
-		$("#shop-count").fadeIn("slow");
 	} else {
 		document.getElementById("shop-container").hidden = true;
-		document.getElementById("shop-count").hidden = true;
+	}
+	
+	if (canrefine) {
+		$("#refine-container").fadeIn("slow");
+	} else {
+		document.getElementById("refine-container").hidden = true;
+	}
+	
+	if (refined == 1) {
+		$("#refined-count").fadeIn("slow");
+		document.getElementById("refined-count").innerHTML = "1 refined material";
+	} else {
+		if (refined != 0) {
+			$("#refined-count").fadeIn("slow");
+			document.getElementById("refined-count").innerHTML = refined + " refined materials";
+		} else {
+			document.getElementById("refined-count").hidden = true;
+		}
 	}
 	
 	document.getElementById("shop-cost").innerHTML = "$" + (shopcost / 2) + " " + (shopcost / 2) + " materials";
 	
 	if (shopcount == 1) {
 		document.getElementById("shop-count").innerHTML = "1 shop";
+		$("#shop-count").fadeIn("slow");
 	} else {
-		document.getElementById("shop-count").innerHTML = shopcount + " shops";
+		if (shopcount != 1) {
+			document.getElementById("shop-count").innerHTML = shopcount + " shops";
+			$("#shop-count").fadeIn("slow");
+		} else {
+			document.getElementById("shop-count").hidden = true;
+		}
 	}
 	
 	if (workeradd == 1) {
@@ -66,10 +107,55 @@ function update() {
 		document.getElementById("worker-desc").innerHTML = "Mines " + workeradd + " materials per second";
 	}
 	
+	objectives();
+	
 	setUpgradeText();
 }
 
-function autosell() {
+function objectives() {
+	if (objective == 0) {
+		document.getElementById("objective").innerHTML = "Get 10 materials";
+		document.getElementById("objective-progress").innerHTML = materials + " / 10";
+		document.getElementById("objective-bar").style.width = ((materials / 10) * 100) + "%";
+		document.getElementById("objective-reward").innerHTML = "Reward: $30";
+		
+		if (materials >= 10) {
+			objective++;
+			money += 30;
+		}
+	}
+	
+	if (objective == 1) {
+		document.getElementById("objective").innerHTML = "Hire 5 workers";
+		document.getElementById("objective-progress").innerHTML = workercount + " / 5";
+		document.getElementById("objective-bar").style.width = ((workercount / 5) * 100) + "%";
+		document.getElementById("objective-reward").innerHTML = "Reward: $50";
+		
+		if (materials >= 10) {
+			objective++;
+			money += 50;
+		}
+	}
+	
+	if (objective == 2) {
+		document.getElementById("objective").innerHTML = "Build 1 shop";
+		document.getElementById("objective-progress").innerHTML = shopcount + " / 1";
+		document.getElementById("objective-bar").style.width = ((shopcount / 1) * 100) + "%";
+		document.getElementById("objective-reward").innerHTML = "Reward: 3 workers";
+		
+		if (materials >= 10) {
+			objective++;
+			
+			addworker(); addworker(); addworker();
+		}
+	}
+	
+	if (objective == 3) {
+		document.getElementsByClassName("objective-container").item(0).hidden = true;
+	}
+}
+
+function autoselltoggle() {
 	autosell = !autosell;
 }
 
@@ -103,9 +189,42 @@ function setUpgradeText() {
 		document.getElementById("upgrade-cost").innerHTML = "Costs: $1000";
 		document.getElementById("upgrade-desc").innerHTML = "Every worker gets a companion dog... +2 materials per second for workers";
 	}
+	
+	if (upgrade == 5) {
+		document.getElementById("upgrade-title").innerHTML = "Refining";
+		document.getElementById("upgrade-cost").innerHTML = "Costs: $5000";
+		document.getElementById("upgrade-desc").innerHTML = "Allows you to refine materials into refined materials, you can use them for things later...";
+	}
+	
+	if (upgrade == 6) {
+		//not added yet
+		//make upgrade panel disappear
+		document.getElementsByClassName("upgrade").item(0).hidden = true;	
+	}
 }
 
 function run() {
+	load();
+	
+	update();
+	var interval = setInterval(function(){
+		update();
+	}, 1);
+	
+	$("#saved").hide();
+	
+	for (var i = 0; i < workercount; i++) {
+		var interval = setInterval(function(){
+			materials += workeradd;
+		}, 1000);
+	}
+	
+	var interval2 = setInterval(function(){
+		save();
+	}, 5000);
+}
+
+function load() {
 	if (localStorage.getItem('materials')) materials = JSON.parse(localStorage.getItem('materials'));
 	if (localStorage.getItem('money')) money = JSON.parse(localStorage.getItem('money'));
 	if (localStorage.getItem('workercount')) workercount = JSON.parse(localStorage.getItem('workercount'));
@@ -118,17 +237,10 @@ function run() {
 	if (localStorage.getItem('shopcount')) shopcount = JSON.parse(localStorage.getItem('shopcount'));
 	if (localStorage.getItem('shopcost')) shopcost = JSON.parse(localStorage.getItem('shopcost'));
 	if (localStorage.getItem('exchange')) exchange = JSON.parse(localStorage.getItem('exchange'));
-	
-	update();
-	var interval = setInterval(function(){
-		update();
-	}, 1);
-	
-	$("#saved").hide();
-	
-	var interval2 = setInterval(function(){
-		save();
-	}, 5000);
+	if (localStorage.getItem('refined')) refined = JSON.parse(localStorage.getItem('refined'));
+	if (localStorage.getItem('canrefine')) canrefine = JSON.parse(localStorage.getItem('canrefine'));
+	if (localStorage.getItem('refinecost')) refinecost = JSON.parse(localStorage.getItem('refinecost'));
+	if (localStorage.getItem('objective')) objective = JSON.parse(localStorage.getItem('objective'));
 }
 
 function save() {
@@ -144,13 +256,12 @@ function save() {
 	localStorage.setItem('shopcount', JSON.stringify(shopcount));
 	localStorage.setItem('shopcost', JSON.stringify(shopcost));
 	localStorage.setItem('exchange', JSON.stringify(exchange));
-	
-	for (var i = 0; i < workercount; i++) {
-		var interval = setInterval(function(){
-			materials += workeradd;
-		}, 1000);
-	}
-	
+	localStorage.setItem('refined', JSON.stringify(refined));
+	localStorage.setItem('canrefine', JSON.stringify(canrefine));
+	localStorage.setItem('refinecost', JSON.stringify(refinecost));
+	localStorage.setItem('objective', JSON.stringify(objective));
+
+
 	$("#saved").fadeIn("slow", function(){ setTimeout(function(){ $("#saved").fadeOut("slow");}, 500); });
 }
 
@@ -175,18 +286,29 @@ function sellten() {
 	if (materials >= 10) sell(10);
 }
 
+function refine() {
+	if (materials >= refinecost) {
+		materials -= refinecost;
+		refined++;	
+	}
+}
+
 function worker() {
 	if (money >= workercost) {
 		money -= workercost;
 		
-		workercost *= 2;
-		
-		workercount++;
-		
-		var interval = setInterval(function(){
-			materials += workeradd;
-		}, 1000);
+		addworker();
 	}
+}
+
+function addworker() {
+	workercost *= 2;
+		
+	workercount++;
+		
+	var interval = setInterval(function(){
+		materials += workeradd;
+	}, 1000);
 }
 
 function shop() {
@@ -250,6 +372,16 @@ function buyupgrade() {
 			money -= 1000;
 			
 			workeradd += 2;
+			
+			enough = true;
+		}
+	}
+	
+	if (upgrade == 5) {
+		if (money >= 5000) {
+			money -= 5000;
+			
+			canrefine = true;
 			
 			enough = true;
 		}
